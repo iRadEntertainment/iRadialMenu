4984a0082d1e3be4e4b5e68bf819c2841ee991cb
Dario De Vita <devitadario@gmail.com>
2025-04-04 06:10:25 +0200

    feat: update plugin configuration and enhance custom type management for RadialMenuComponent

--- addons/RadialMenuComponent/plugin.gd
+++ addons/RadialMenuComponent/plugin.gd
@@ -1,12 +1,25 @@
 @tool
 extends EditorPlugin
 
+# Called when the plugin is enabled
+func _enter_tree() -> void:
+	# Add custom types
+	add_custom_type(
+		"RadialMenu2D",
+		"Control",
+		preload("RadialMenu2D.gd"),
+		preload("icon_radial_2d.svg")
+	)
+	add_custom_type(
+		"RadialMenu3DFlat",
+		"Node3D",
+		preload("RadialMenu3DFlat.gd"),
+		preload("icon_radial_3d_2d.svg")
+	)
 
-func _enter_tree():
-	add_custom_type("RadialMenu2D", "Control", preload("RadialMenu2D.gd"), preload("icon_radial_2d.svg"))
-	add_custom_type("RadialMenu3DFlat", "Control", preload("RadialMenu3DFlat.gd"), preload("icon_radial_3d_2d.svg"))
 
-
-func _exit_tree():
+# Called when the plugin is disabled
+func _exit_tree() -> void:
+	# Remove custom types
 	remove_custom_type("RadialMenu2D")
 	remove_custom_type("RadialMenu3DFlat")
